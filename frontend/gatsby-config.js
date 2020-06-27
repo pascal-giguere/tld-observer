5495d3ef586abb5e25571ee48d0183b48a60b03a
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2020-06-26 21:32:47 -0400

    set up basic dependencies

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,10 +1,14 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-styled-components',
+    },
+  ],
+};
