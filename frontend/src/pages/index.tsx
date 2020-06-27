7caa70de6b7132e729b178fa8865449e7fc09842
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2020-06-27 09:18:56 -0400

    global style

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #f7ef80;
+  }
+`;
 
 export default function Home() {
   return (
@@ -9,6 +16,7 @@ export default function Home() {
         <title>TLD Observer</title>
         <link rel='canonical' href='https://tld.observer/' />
       </Helmet>
+      <GlobalStyle />
       <div>Hello world!</div>
     </React.Fragment>
   );
