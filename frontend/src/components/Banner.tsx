c2f2f42e804dbe7dbedc8157f1d9155c20f6b1a9
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2020-07-19 15:11:18 -0400

    banner styling

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  padding: 10px 0;
+  border-bottom: 2px solid black;
+`;
 
 interface Props {
   children: string;
