a460c3613c6284f18bfadffc9f63f8ddfe3a7e7d
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2020-07-26 18:36:58 -0400

    add junction table migration

--- backend/migrations/20200726161320-create-topic-table.ts
+++ backend/migrations/20200726161320-create-topic-table.ts
@@ -6,8 +6,7 @@ export function up(db: PgDriver, callback: () => void): void {
   db.createTable(
     TABLE_NAME,
     {
-      id: { type: 'uuid', primaryKey: true, notNull: true },
-      key: { type: 'string', unique: true, notNull: true },
+      key: { type: 'string', primaryKey: true, notNull: true },
     },
     callback
   );
---
+++ backend/migrations/20200726174403-create-member-topic-junction-table.ts
@@ -0,0 +1,56 @@
+import { PgDriver } from 'db-migrate-pg';
+
+const JUNCTION_TABLE_NAME = 'member_topic';
+const JUNCTION_FK1 = {
+  fkName: 'junction-member-fk',
+  keyName: 'memberId',
+  refTableName: 'member',
+  refKeyName: 'id',
+  refKeyType: 'uuid',
+};
+const JUNCTION_FK2 = {
+  fkName: 'junction-topic-fk',
+  keyName: 'topicKey',
+  refTableName: 'topic',
+  refKeyName: 'key',
+  refKeyType: 'string',
+};
+
+export function up(db: PgDriver, callback: () => void): void {
+  db.createTable(
+    JUNCTION_TABLE_NAME,
+    {
+      [JUNCTION_FK1.keyName]: { type: JUNCTION_FK1.refKeyType, notNull: true },
+      [JUNCTION_FK2.keyName]: { type: JUNCTION_FK2.refKeyType, notNull: true },
+    },
+    () => {
+      db.addForeignKey(
+        JUNCTION_TABLE_NAME,
+        JUNCTION_FK1.refTableName,
+        JUNCTION_FK1.fkName,
+        { [JUNCTION_FK1.keyName]: JUNCTION_FK1.refKeyName },
+        {
+          onDelete: 'RESTRICT',
+          onUpdate: 'CASCADE',
+        },
+        () => {
+          db.addForeignKey(
+            JUNCTION_TABLE_NAME,
+            JUNCTION_FK2.refTableName,
+            JUNCTION_FK2.fkName,
+            { [JUNCTION_FK2.keyName]: JUNCTION_FK2.refKeyName },
+            {
+              onDelete: 'RESTRICT',
+              onUpdate: 'CASCADE',
+            },
+            callback
+          );
+        }
+      );
+    }
+  );
+}
+
+export function down(db: PgDriver, callback: () => void): void {
+  db.dropTable(JUNCTION_TABLE_NAME, callback);
+}
