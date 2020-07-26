db48de33ffd81ac1936ab3e2112f7320d856dde2
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2020-07-26 14:30:03 -0400

    add sumologic transport

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,10 +1,24 @@
-import winston from 'winston';
+import env from 'env-var';
+import winston, { transport as Transport } from 'winston';
+import { SumoLogic } from 'winston-sumologic-transport';
 
-export const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.prettyPrint(),
-      level: 'debug',
-    }),
-  ],
-});
+const sumoLogicCollectorUrl: string | undefined = env.get('SUMOLOGIC_COLLECTOR_URL').asUrlString();
+const sumoLogicEnabled: boolean = typeof sumoLogicCollectorUrl !== 'undefined';
+
+const transports: Transport[] = [
+  new winston.transports.Console({
+    format: winston.format.cli(),
+    level: 'debug',
+  }),
+];
+
+if (sumoLogicEnabled) {
+  transports.push(
+    new SumoLogic({
+      url: sumoLogicCollectorUrl,
+      level: 'info',
+    })
+  );
+}
+
+export const logger = winston.createLogger({ transports });
