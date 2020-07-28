88d8fe9027cc8a68f288c69c2490a9574ccb5a57
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2020-07-28 16:05:38 -0400

    improve error logging

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,7 +1,7 @@
 import env from 'env-var';
 import winston, { transport as Transport } from 'winston';
 import { SumoLogic } from 'winston-sumologic-transport';
-import { stringify } from 'flatted';
+import stringify from 'json-stringify-safe';
 
 const sumoLogicCollectorUrl: string | undefined = env.get('SUMOLOGIC_COLLECTOR_URL').asUrlString();
 const sumoLogicEnabled: boolean = typeof sumoLogicCollectorUrl !== 'undefined';
@@ -9,15 +9,18 @@ const sumoLogicEnabled: boolean = typeof sumoLogicCollectorUrl !== 'undefined';
 const transports: Transport[] = [
   new winston.transports.Console({
     format: winston.format.combine(
-      winston.format.timestamp({ format: 'DD-MM-YYYY H:ss' }),
+      winston.format.timestamp({ format: 'DD-MM-YYYY H:mm:ss' }),
       winston.format.cli(),
       winston.format.metadata(),
       winston.format.printf((info) => {
-        const { timestamp, ...metadata } = info.metadata;
-        const formattedMetadata: string = Object.keys(metadata).length
-          ? `— Metadata: ${stringify(metadata, null, 2)}`
-          : '';
-        return `${timestamp} ${info.level} ${info.message} ${formattedMetadata}`;
+        const { timestamp, error, ...metadata } = info.metadata;
+        let formattedError = '';
+        if (error) {
+          const { name, message, stack } = error;
+          formattedError = ` — \x1b[31mError: ${stringify({ name, message, stack })}\x1b[0m`;
+        }
+        const formattedMetadata: string = Object.keys(metadata).length ? ` — Metadata: ${stringify(metadata)}` : '';
+        return `${timestamp} ${info.level} ${info.message}${formattedError}${formattedMetadata}`;
       })
     ),
     level: 'debug',
