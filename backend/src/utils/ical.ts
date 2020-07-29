fa1340b045540af451666b2800d61447c2dcf814
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2020-07-29 17:32:56 -0400

    ical util wip

---
+++ backend/src/utils/ical.ts
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import env from 'env-var';
+import ical, { CalendarComponent, FullCalendar } from 'ical';
+
+const sunriseIcsUrl: string = env.get('SUNRISE_ICS_URL').required().asUrlString();
+
+enum CalendarType {
+  sunrise = 'sunrise',
+  generalAvailability = 'generalAvailability',
+}
+
+export async function processSunriseCalendar(): Promise<void> {
+  const calendar: FullCalendar = await fetchCalendar(sunriseIcsUrl);
+  console.log(calendar);
+  Object.values(calendar).forEach((event: CalendarComponent) => {
+    console.log(event);
+  });
+}
+
+async function fetchCalendar(icsFileUrl: string): Promise<FullCalendar> {
+  const icsData: string = await fetchFile(icsFileUrl);
+  return ical.parseICS(icsData);
+}
+
+async function fetchFile(url: string): Promise<string> {
+  const response = await axios.get(url);
+  return response.data;
+}
+
+class TldCalendarEvent {
+  tld: string;
+  sunriseEndDate?: Date;
+  generalAccessStartDate?: Date;
+
+  constructor(tld: string, sunriseEndDate?: Date, generalAccessStartDate?: Date) {
+    this.tld = tld;
+    this.sunriseEndDate = sunriseEndDate;
+    this.generalAccessStartDate = generalAccessStartDate;
+  }
+
+  static fromCalendarComponent(calendarComponent: CalendarComponent): TldCalendarEvent {
+    return new TldCalendarEvent('TODO');
+  }
+
+  private setDateFromComponent(calendarComponent: CalendarComponent): void {
+    const calendarType: CalendarType = TldCalendarEvent.findCalendarType(calendarComponent);
+    switch (calendarType) {
+      case CalendarType.sunrise: {
+        this.sunriseEndDate = calendarComponent.end;
+        return;
+      }
+      case CalendarType.generalAvailability: {
+        this.generalAccessStartDate = calendarComponent.start;
+        return;
+      }
+    }
+  }
+
+  private static findCalendarType(calendarComponent: CalendarComponent): CalendarType {
+    // TODO
+    return CalendarType.generalAvailability;
+  }
+}
--- backend/src/worker.ts
+++ backend/src/worker.ts
@@ -1,13 +1,9 @@
-import { AxiosResponse } from 'axios';
-import { apiClient } from '@utils/api';
-import { logger } from '@utils/logger';
-import { IMember } from '@common/interfaces';
+import { processSunriseCalendar } from '@utils/ical';
 
 console.log('Hello world, worker!');
 
 async function startWorker(): Promise<void> {
-  const response: AxiosResponse<IMember[]> = await apiClient.get('/member');
-  logger.info('Worker retrieved members!', { members: response.data });
+  await processSunriseCalendar();
 }
 
 (async () => startWorker())();
