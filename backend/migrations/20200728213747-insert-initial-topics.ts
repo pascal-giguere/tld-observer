ca010331b548a427842632119a596068c3631778
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2020-07-28 21:47:37 -0400

    add initial topics migration

--- backend/migrations/20200728213747-insert-initial-topics.ts
+++ backend/migrations/20200728213747-insert-initial-topics.ts
@@ -1,19 +1,21 @@
 import { PgDriver } from 'db-migrate-pg';
 
-const TABLE_NAME = 'member';
+const TABLE_NAME = 'topic';
+const COL_NAME = 'key';
+const TOPIC_KEYS = ['newTlds', 'upcomingTlds'];
+
+const SQL_INSERT_QUERY = `INSERT INTO ${TABLE_NAME}(${COL_NAME}) VALUES ${TOPIC_KEYS.map(
+  (key: string) => `('${key}')`
+).join(',')};`;
+
+const SQL_DELETE_QUERY = `DELETE FROM ${TABLE_NAME} WHERE ${COL_NAME} IN (${TOPIC_KEYS.map(
+  (key: string) => `'${key}'`
+).join(',')});`;
 
 export function up(db: PgDriver, callback: () => void): void {
-  db.createTable(
-    TABLE_NAME,
-    {
-      id: { type: 'uuid', primaryKey: true, notNull: true },
-      email: { type: 'string', unique: true, notNull: true },
-      name: { type: 'string', notNull: true },
-    },
-    callback
-  );
+  db.runSql(SQL_INSERT_QUERY, callback);
 }
 
 export function down(db: PgDriver, callback: () => void): void {
-  db.dropTable(TABLE_NAME, callback);
+  db.runSql(SQL_DELETE_QUERY, callback);
 }
