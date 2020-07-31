52f07201c155e4145e199865a3190004384bca73
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2020-07-31 13:45:44 -0400

    send emails with mailgun

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -1,9 +1,14 @@
+import env from 'env-var';
+import mailgun, { Mailgun } from 'mailgun-js';
 import { findMembersWithTopicKey } from '@utils/api';
 import { logger } from '@utils/logger';
 import { IMember } from '@common/interfaces';
 import { TopicKey } from '@common/enums';
 
-export async function sendWelcomeEmail(memberName: string, memberEmail: string): Promise<void> {}
+const apiKey: string = env.get('MAILGUN_KEY').required().asString();
+const domain: string = env.get('MAILGUN_DOMAIN').required().asString();
+
+const mailgunClient: Mailgun = new mailgun({ apiKey, domain });
 
 export async function sendLaunchingTldEmails(tld: string): Promise<void> {
   const members: IMember[] = await findMembersWithTopicKey(TopicKey.newTlds);
@@ -11,10 +16,6 @@ export async function sendLaunchingTldEmails(tld: string): Promise<void> {
     await sendLaunchedTldEmail(member.name, member.email, tld);
   }
   logger.info('Sent launching TLD emails', { tld, memberCount: members.length });
-}
-
-async function sendLaunchedTldEmail(memberName: string, memberEmail: string, tld: string): Promise<void> {
-  // TODO
 }
 
 export async function sendUpcomingTldEmails(tld: string, launchDate: Date): Promise<void> {
@@ -25,6 +26,18 @@ export async function sendUpcomingTldEmails(tld: string, launchDate: Date): Prom
   logger.info('Sent upcoming TLD emails', { tld, launchDate, memberCount: members.length });
 }
 
+export async function sendWelcomeEmail(memberName: string, memberEmail: string): Promise<void> {
+  // TODO
+  const emailSubject = 'Welcome to TLD Observer';
+  await sendEmail(memberEmail, emailSubject, 'TODO');
+}
+
+async function sendLaunchedTldEmail(memberName: string, memberEmail: string, tld: string): Promise<void> {
+  // TODO
+  const emailSubject = `New '${tld}' TLD is launching today! 🚀`;
+  await sendEmail(memberEmail, emailSubject, 'TODO');
+}
+
 async function sendUpcomingTldEmail(
   memberName: string,
   memberEmail: string,
@@ -32,8 +45,16 @@ async function sendUpcomingTldEmail(
   launchDate: Date
 ): Promise<void> {
   // TODO
+  const emailSubject = `New '${tld}' TLD is launching soon! 🚀`;
+  await sendEmail(memberEmail, emailSubject, 'TODO');
 }
 
-/*
-async function sendEmail(emailAddress: string, emailTitle: string, emailBodyHtml: string): Promise<void> {}
-*/
+async function sendEmail(emailAddress: string, emailSubject: string, emailBodyHtml: string): Promise<void> {
+  const response: mailgun.messages.SendResponse = await mailgunClient.messages().send({
+    from: 'TLD Observer <noreply@tld.observer>',
+    to: emailAddress,
+    subject: emailSubject,
+    html: emailBodyHtml,
+  });
+  logger.debug('Successfully sent email', { emailAddress, emailSubject, emailBodyHtml, response });
+}
